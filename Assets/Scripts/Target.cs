a81c5b76e5aa7d9d8c11deb56eb6c663cbfde7b7
Danna <danna.wa@kakaobrain.com>
2022-10-02 22:53:07 +0900

    feat: GameController 추가

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -8,12 +8,17 @@ public class Target : MonoBehaviour
     private void OnEnable()
     {
         currentHealth = health;
+        GameControl.main.AddTargetToList(this);
     }
 
     public void OnTargetHit(int damage)
     {
         currentHealth -= damage;
-        
-        if (currentHealth < 0) Destroy(gameObject);
+
+        if (currentHealth < 0)
+        {
+            GameControl.main.DeleteTargetFromList(this);
+            Destroy(gameObject);
+        }
     }
 }
